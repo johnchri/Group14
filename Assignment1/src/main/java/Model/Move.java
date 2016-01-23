190ff4e9b2f9fd6637dd2fbfd07150137cbced43
johnchri <johnchri@oregonstate.edu>
2016-01-22 19:39:50 -0800

    added to move function
    added to move function

--- Assignment1/src/main/java/Model/Move.java
+++ Assignment1/src/main/java/Model/Move.java
@@ -3,9 +3,14 @@ package Model;
 /**
  * Created by chkyj_000 on 1/22/2016.
  */
-public class Move {
+public class move {
 
-    from[] = {0,0};
-    to[] = {0,0};
+    public array from[2] = {0,0};
+    public array to[2] = {0,0};
+
+    public move(int x1, int y1, int x2, int y2){
+        from[2] = {x1, y1};
+        to[2] = {x2, y2};
+    }
 
 }
